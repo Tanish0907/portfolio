034d95aec893cb0661c071a0786d22cb41c955a3
Tanish0907 <sharmatanish097654@gmail.com>
2023-02-17 21:37:01 +0530

    update

---
+++ js/scroll.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.intro', { delay: 150 , reset: true});
+ScrollReveal().reveal('.aboutme', { delay: 100,reset: true });
+ScrollReveal().reveal('.services', { delay: 100 , reset: true});
+ScrollReveal().reveal('.projects', { delay: 0 , reset: true});
+ScrollReveal().reveal('.projcard', { delay: 5 , reset: true});
